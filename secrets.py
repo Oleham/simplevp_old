747247d4c9071bf9ed6436753643e2d579aaf2d1
Ole Hammersland <63668718+Oleham@users.noreply.github.com>
2021-11-08 22:50:20 +0100

    Delete secrets.py

--- secrets.py
+++
@@ -1,7 +0,0 @@
-secrets = {
-    "xtrf": "https://xtrf.globalscandinavia.com",
-    "email": "post@olehammersland.com",
-    "pw": "456AdriaterPyttVann%",
-    "ppf": "C:\\Users\\oleham\\Documents\\daimler\\ppf",
-    "tpf": "C:\\Users\\oleham\\Documents\\daimler\\tpf"
-}
